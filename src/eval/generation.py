153a3cd5b9ba660f2c168f36c49c15eaf26e4db6
00000000014B3659 <cisem.altan@tum.de>
2024-12-15 13:11:02 +0100

    small fix

--- src/eval/generation.py
+++ src/eval/generation.py
@@ -9,26 +9,25 @@ from settings import VIGNETTE_COLLECTION
 from prompts import GENERATION_EVALUATION_PROMPT
 
 
-def evaluate_single(vignette_id: int, question: str, faiss_service: FaissService, top_k: int = 3) -> Feedback:
+def evaluate_single(vignette_id: int, question_id: int, faiss_service: FaissService, top_k: int = 3) -> Feedback:
     vignette = VIGNETTE_COLLECTION.get_vignette_by_id(vignette_id)
     questions = vignette.get_questions()
-
+    print(f"Questions in vignette {vignette_id}: {len(questions)}")
     if questions is None:
         print(f"Vignette with id {vignette_id} not found")
         return None
 
-    background = None
-    answer = None
-    for question_obj in questions:
-        if question == question_obj.question:
-            question_id = question_obj.id  # tODO: get id directly instead of matching the str query
-            break
+    background = vignette.get_background()
 
+    question = vignette.get_question(question_id)
+    answer = question.get_answer()
+
+    print(f"Question {question_id}: {question.get_question()}")
     if background is None or answer is None:
-        print(f"Question not found in vignette {vignette_id}")
+        print(f"Either background or answer not found for vignette id {vignette_id} and question id {question_id}")
         return None
 
-    retrieved_documents = retrieve(question, faiss_service, top_k=top_k)
+    retrieved_documents = retrieve(question.get_question(), faiss_service, top_k=top_k)
     prompt = create_question_prompt(retrieved_documents, vignette, question_id)
 
     response = generate_response(prompt)
@@ -38,7 +37,7 @@ def evaluate_single(vignette_id: int, question: str, faiss_service: FaissService
             Related information:\n{''.join([f"{docu}\n" for docu in retrieved_documents])}
                     
             Background:\n{background}
-            Question:\n{question}
+            Question:\n{question.get_question()}
             """,
         response=response,
         reference_answer=answer,
@@ -63,9 +62,9 @@ def evaluate_source(
             if question.get_source() != source:
                 continue
             if text_only and question.text_only:
-                all_feedbacks.append(evaluate_single(vignette.get_id(), question.get_question(), faiss_service, top_k))
+                all_feedbacks.append(evaluate_single(vignette.get_id(), question.get_id(), faiss_service, top_k))
             elif not text_only:
-                all_feedbacks.append(evaluate_single(vignette.get_id(), question.get_question(), faiss_service, top_k))
+                all_feedbacks.append(evaluate_single(vignette.get_id(), question.get_id(), faiss_service, top_k))
             else:
                 pass
 
